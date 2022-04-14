5f7b25173e3d43f5c2911ab336f78883ef8da9a6
caioalbert <caioalberto2104@gmail.com>
2022-04-13 22:02:53 -0300

    tarefa-1 ok

---
+++ tarefa-1.rb
@@ -0,0 +1,9 @@
+
+/*exibe uma a mensagem ao usuario*\
+print "digite seu nome:"
+
+/*recebe uma entrada do tioo string do  nome do usuario*\
+nome = gets.chomp
+
+/*exibe mensagem comprimentando o mesmo*\
+puts "olá, #{nome} muito prazer em ter você conosco"
