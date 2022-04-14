ce5e9a1b0cced1a7fa72caa420b02e00280b1f9d
caioalbert <55882790+caioalbert@users.noreply.github.com>
2022-04-13 22:17:46 -0300

    adicionando comentarios

--- tarefa-1.rb
+++ tarefa-1.rb
@@ -1,9 +1,9 @@
 
-/*exibe uma a mensagem ao usuario*\
+#exibe uma a mensagem ao usuario
 print "digite seu nome:"
 
-/*recebe uma entrada do tipo string do  nome do usuario*\
+#recebe uma entrada do tipo string do  nome do usuario
 nome = gets.chomp
 
-/*exibe mensagem comprimentando o mesmo*\
+#exibe mensagem comprimentando o mesmo
 puts "olá, #{nome} muito prazer em ter você conosco"
