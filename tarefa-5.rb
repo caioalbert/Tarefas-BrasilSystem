27c3352334295a81d968874f3b7e651446c5019c
caioalbert <55882790+caioalbert@users.noreply.github.com>
2022-04-16 15:17:54 -0300

    Update tarefa-5.rb

--- tarefa-5.rb
+++ tarefa-5.rb
@@ -2,36 +2,25 @@
 print "Digite um DDD para saber a que cidade pertence: "
 
 #aguarda a entrada do usuario
-DDD = gets.chomp.to_i
+ddd = gets.chomp.to_i
 
-#
-case DDD
+puts "============================"
+
+case ddd
 when 61
-   puts  "============================"
    puts  "Este DDD pertence a Brasilia"
-   puts  "============================"
 when 71
-    puts "============================"
-    puts "Este DDD pertence a Salvado"
-    puts "============================"
+    puts "Este DDD pertence a Salvador"
 when 11
-    puts "=============================="
     puts "Este DDD pertence a São Paulo"
-    puts "=============================="
 when 21
-    puts "=================================="
     puts "Este DDD pertence a Rio de Janeiro"
-    puts "=================================="
 when 32
-    puts "================================"
     puts "Este DDD pertence a Juiz de Fora"
-    puts "================================"
 when 19
-    puts "============================"
     puts "Este DDD pertence a Campinas"
-    puts "============================"
 else
-    puts "============================"
-    puts "      DDD não cadastrado    "
-    puts "============================"
+    puts "DDD não cadastrado    "
 end
+
+puts "============================"
