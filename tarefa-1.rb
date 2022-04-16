d6a3a8ffc65f79ecd2183b8a42fbee355015660d
caioalbert <55882790+caioalbert@users.noreply.github.com>
2022-04-16 15:03:53 -0300

    Update tarefa-1.rb

--- tarefa-1.rb
+++ tarefa-1.rb
@@ -1,8 +1,8 @@
 #exibe uma a mensagem ao usuario
-print "digite seu nome:"
+print "digite seu nome: "
 
 #recebe uma entrada do tipo string do  nome do usuario
 name = gets.chomp
 
 #exibe mensagem comprimentando o mesmo
-puts "olá, #{name} muito prazer em ter você conosco"
+puts "olá #{name}, muito prazer em ter você conosco"
