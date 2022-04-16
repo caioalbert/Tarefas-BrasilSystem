641aa6b2e62b7fcd4c7c2cd39e302ee3e3be3614
caioalbert <55882790+caioalbert@users.noreply.github.com>
2022-04-16 14:37:43 -0300

    Update tarefa-2.rb

--- tarefa-2.rb
+++ tarefa-2.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #mensagem de saida exibida ao usuario
 print "digite a mensagem que deseja inverter:"
 
@@ -7,13 +6,3 @@ mensagem = gets.chomp
 
 #exibição da mensagem invertida
 puts mensagem.reverse
-=======
-#mensagem de saida exibida ao usuario
-print "digite a mensagem que deseja inverter:"
-
-#entrada da mensagem que o usuario deseja inverter
-mensagem = gets.chomp
-
-#exibição da mensagem invertida
-puts mensagem.reverse
->>>>>>> 35c17a0b140276e6c5631601e8b82257be4b4f98
